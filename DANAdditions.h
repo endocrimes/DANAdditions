7fc6b80017298372b9ada10c8035e2f86cd936c5
Danielle Lancashire <dani@builds.terrible.systems>
2014-01-02 00:17:09 +0000

    Initial Commit

---
+++ DANAdditions.h
@@ -0,0 +1,17 @@
+//
+//  DANAdditions.h
+//
+//  Copyright (c) 2014 Danielle Lancashire. All rights reserved.
+//
+
+#ifndef DANAdditions_h
+#define DANAdditions_h
+
+#import "NSString+DANAdditions.h"
+#import "UIColor+DANAdditions.h"
+#import "UINavigationController+DANAdditions.h"
+#import "UIView+DANAdditions.h"
+#import "NSDate+DANAdditions.h"
+
+
+#endif
---
+++ UIColor+DANAdditions.m
@@ -0,0 +1,86 @@
+//
+//  UIColor+DANAdditions.m
+//
+//  Copyright (c) 2014 Danielle Lancashire. All rights reserved.
+//
+
+#import "UIColor+DANAdditions.h"
+
+@implementation UIColor (DANAdditions)
+
++(UIColor *) colorFromHexString:(NSString *)hexString 
+{
+    unsigned rgbValue = 0;
+    hexString = [hexString stringByReplacingOccurrencesOfString:@"#" withString:@""];
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    
+    [scanner scanHexInt:&rgbValue];
+    
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+}
+
+- (UIColor *) lighten:(float)amount
+{
+    float h, s, b, a;
+    if ([self getHue:&h saturation:&s brightness:&b alpha:&a])
+        return [UIColor colorWithHue:h
+                          saturation:s
+                          brightness:(b + (b * (amount / 100)))
+                               alpha:a];
+    return nil;
+}
+
+- (UIColor *) darken:(float)amount
+{
+    float h, s, b, a;
+    if ([self getHue:&h saturation:&s brightness:&b alpha:&a])
+        return [UIColor colorWithHue:h
+                          saturation:s
+                          brightness:(b - (b * (amount / 100)))
+                               alpha:a];
+    return nil;
+}
+
+- (UIColor *) saturate:(float)amount 
+{
+    float h, s, b, a;
+    if ([self getHue:&h saturation:&s brightness:&b alpha:&a])
+        return [UIColor colorWithHue:h
+                          saturation:(s + (s * (amount / 100)))
+                          brightness:b
+                               alpha:a];
+    return nil;
+}
+
+- (UIColor *) desaturate:(float)amount 
+{
+    float h, s, b, a;
+    if ([self getHue:&h saturation:&s brightness:&b alpha:&a])
+        return [UIColor colorWithHue:h
+                          saturation:(s - (s * (amount / 100)))
+                          brightness:b
+                               alpha:a];
+    return nil;
+}
+
+- (UIColor *) invert 
+{
+    float r, g, b, a;
+    if ([self getRed:&r green:&g blue:&b alpha:&a])
+        return [UIColor colorWithRed:1.0-r
+                               green:1.0-g
+                                blue:1.0-b
+                               alpha:a];
+    return nil;
+}
+
+- (UIColor *) greyscale 
+{
+    float r, g, b, a;
+    if ([self getRed:&r green:&g blue:&b alpha:&a])
+        return [UIColor colorWithWhite:((r + g + b)/3)
+                                 alpha:a];
+    return nil;
+}
+
+@end
